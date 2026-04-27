a37b7c483a0feffb90f5a564b15a61e93e92aa0d
eldonjmcclure-ops <eldonjmcclure@gmail.com>
2026-04-27 14:50:58 -0600

    added DOM project

---
+++ ponder/dom.js
@@ -0,0 +1,23 @@
+let title = document.querySelector('h1');
+
+console.log(title);
+
+title.textContent = 'Web Page Components';
+
+document.querySelector('#topics').style.color = 'red';
+
+// const should be for things you are never going to change.
+const topics = document.querySelector('#topics');
+
+topics.style.color = 'purple'
+
+const wrapper = document.getElementById("content");
+
+wrapper.style.backgroundColor = 'lightblue';
+
+let list = document.querySelector('.list')
+
+list.style.border = "3px solid black";
+
+let para = document.querySelector('p');
+para.style.fontSize = '2em';
