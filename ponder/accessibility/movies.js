f80b8e21ba7fdbcafd22891891175b0b5e1f99fe
eldonjmcclure-ops <eldonjmcclure@gmail.com>
2026-05-29 22:10:08 -0600

    updated blog folder with JS

--- ponder/accessibility/movies.js
+++ ponder/accessibility/movies.js
@@ -51,7 +51,7 @@ const movies = [
         article.className = "movie"
         
         let html = `
-            <h2>${movie.title}</h2>
+          <h2>${movie.title}</h2>
             <img src=${movie.imgSrc} alt="${movie.imgAlt}">
             <p><strong>Release Date:</strong> ${movie.date}</p>
             <p><strong>Recommended Age:</strong> ${movie.ages}</p>
@@ -59,12 +59,6 @@ const movies = [
             <p><strong>Rating:</strong> <span aria-label="${movie.stars.length} out of 5 stars" role="img">${movie.stars}</span></p>
             <p id='desc'>${movie.description}</p>
             `
-
-        article.innerHTML = html
-        movieList.appendChild(article)
-    
+    article.innerHTML = html
+    movieList.appendChild(article)
     });
-          
-
-
-
