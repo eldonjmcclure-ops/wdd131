5be9eb27abade46dc2e344d0d53a41a25caa9b1d
eldonjmcclure-ops <eldonjmcclure@gmail.com>
2026-06-24 15:03:01 -0600

    Added hikes

---
+++ ponder/sortfilter/sortfilter.js
@@ -0,0 +1,30 @@
+
+
+                
+
+
+
+
+
+
+
+
+
+
+// nums = [12, 10, 8, 3]
+
+// console.log(nums.sort(compareFn));
+
+
+// function compareFn(a,b) {
+//   if (a < b) {
+//     return -1;
+//   } else if (a > b) {
+//     return 1;
+//   }
+//  return 0;
+// }
+                
+
+
+// const simpleList = ["oranges", "grapes", "lemons", "apples", "Bananas", "watermelons", "coconuts", "broccoli", "mango"];
